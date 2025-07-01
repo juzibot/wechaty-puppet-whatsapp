ec19668a6d467252e318047f978ab0d04de94856
NickWang <brotherstyx@gmail.com>
2025-07-02 01:20:14 +0800

    feat: 🎸 increase timeout

--- src/config.ts
+++ src/config.ts
@@ -74,10 +74,10 @@ type LangCodes = keyof typeof STRINGS
 export const LANGUAGE: LangCodes = process.env['LANGUAGE'] as LangCodes | undefined || 'zh_CN'
 
 export const DEFAULT_TIMEOUT = {
-  MESSAGE_SEND: 20 * 1000, // should allow long waiting time since connection breaks a log (maybe even no timeout limit?)
+  MESSAGE_SEND: 60 * 1000, // should allow long waiting time since connection breaks a log (maybe even no timeout limit?)
   MESSAGE_SEND_FILE: 15 * 60 * 1000,
-  MESSAGE_SEND_TEXT: 20 * 1000,
-  TIMEOUT_WAIT_CONNECTED: 3 * 1000,
+  MESSAGE_SEND_TEXT: 60 * 1000,
+  TIMEOUT_WAIT_CONNECTED: 10 * 1000,
 }
 
 export const HISTORY_MESSAGES_DAYS = Number(process.env['HISTORY_MESSAGES_DAYS'] || '3')
